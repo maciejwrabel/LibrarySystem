90ccf3d9f671fec064f99d7156245c8de568ce14
Maciej Wrabel <maciejwrabel@interia.pl>
2020-09-09 21:48:41 +0200

    Classes and files has been created.

--- src/LibrarySystem/Library.java
+++ src/LibrarySystem/Library.java
@@ -1,4 +1,63 @@
 package LibrarySystem;
 
+import java.util.Scanner;
+
 public class Library {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int menu;
+
+        while (true) {
+
+            System.out.println("WELCOME IN THE LIBRARY SYSTEM");
+            System.out.println("Please choose one of the following menu options");
+            System.out.println("----------------------------------------------------------------------------------");
+            System.out.println("1 - See all books in library, if you want to borrow one please login to the system");
+            System.out.println("2 - Add new reader account");
+            System.out.println("3 - Login as a reader");
+            System.out.println("4 - For library workers");
+            System.out.println("5 - Close the Library System");
+            System.out.println("----------------------------------------------------------------------------------");
+
+            menu = sc.nextInt();
+
+            if (menu == 4) {
+
+                System.out.println("Choose one of the following option:");
+                System.out.println("-----------------------------");
+                System.out.println("1 - Add new worker accout");
+                System.out.println("2 - Login to worker accout");
+                System.out.println("3 - Back to the main manu");
+                System.out.println("-----------------------------");
+
+                menu = sc.nextInt();
+
+                if (menu == 1){
+                    // add worker
+                }
+                if (menu == 2){
+
+                    // login as a worker
+                }
+
+                if (menu == 3 ){
+                    continue;
+                }
+
+            }
+
+            if (menu == 5) {
+                System.out.println("Thank you for your visit in our Library System");
+                System.out.println("See you next time!!!");
+
+                break;
+
+            }
+
+        }
+
+    }
 }
--- src/LibrarySystem/Reader.java
+++ src/LibrarySystem/Reader.java
@@ -1,4 +1,58 @@
 package LibrarySystem;
 
 public class Reader {
+
+    public String name;
+    public String surname;
+    public String dateOfBirthday;
+    public String address;
+
+    public Reader(String name, String surname, String dateOfBirthday, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirthday = dateOfBirthday;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDateOfBirthday() {
+        return dateOfBirthday;
+    }
+
+    public void setDateOfBirthday(String dateOfBirthday) {
+        this.dateOfBirthday = dateOfBirthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfBirthday='" + dateOfBirthday + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
